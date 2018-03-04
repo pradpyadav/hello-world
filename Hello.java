1a56ab1ff239b86e650595de881c066f97c368a7
Pradeep Yadav <pradpyadav@gmail.com>
2018-03-04 02:37:03 -0600

    Added some Util class and called it from hello class.

--- Hello.java
+++ Hello.java
@@ -7,6 +7,10 @@ public class Hello {
 		for(int i=0;i<10;i++){
 			System.out.println("Hello Wolrd "+i);
 		}
+		System.out.println("Run the show now");
+		UtilLevelClass ulc= new UtilLevelClass();
+		ulc.runTheShow();
+		System.out.println("Show run successful");
 		System.out.println("End of program");
 	}
 
---
+++ UtilLevelClass.java
@@ -0,0 +1,7 @@
+
+public class UtilLevelClass {
+
+	public void runTheShow(){
+		System.out.println("Show is ON Guys");
+	}
+}
